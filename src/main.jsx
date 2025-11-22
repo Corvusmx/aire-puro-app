aec52de4cb13a7a97afcc1fb26cc2deb371e9ee0
Corvusmx <herles@hotmail.com>
2025-11-21 20:14:08 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import QuitApp from './QuitApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <QuitApp />
+  </React.StrictMode>
+);
